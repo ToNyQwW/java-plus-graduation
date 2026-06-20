38423844401662eb4d64b625ac228fc672ce9d52
Konstantin <tenenbaum1994@gmail.com>
2026-06-20 16:03:38 +0700

    фикс

--- core/interaction-api/src/main/java/ru/practicum/exception/ConditionsConflictException.java
+++ core/interaction-api/src/main/java/ru/practicum/exception/ConditionsConflictException.java
@@ -1,7 +1,6 @@
 package ru.practicum.exception;
 
 public class ConditionsConflictException extends RuntimeException {
-
     public ConditionsConflictException(String message) {
         super(message);
     }
